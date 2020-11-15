725fbf810c6c5470d5754c24a5682814026d6c4c
Zoroter <zorotes@gmail.com>
2020-11-15 01:42:42 +0100

    added signup form

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 3, maximum: 25}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 69}, format: { with: VALID_EMAIL_REGEX }
+  before_save { self.email = email.downcase }
+  has_secure_password
 end
